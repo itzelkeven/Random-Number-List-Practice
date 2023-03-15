76b417a546294be28f3843518aefa9186b7af6e1
730443 <730443@605-110-S13>
2023-03-15 09:42:41 -0600

    Add project files.

---
+++ Random_Number_List_Practice.py
@@ -0,0 +1,35 @@
+import random
+bruh = []
+for i in range(12):
+    bruh.append(random.randrange(1,11))
+
+biggest = bruh[0]
+for i in range(12):
+    if bruh[i]>biggest:
+        biggest = bruh[i]
+print(bruh)
+print("Biggest: ", biggest)
+print()
+print()
+smallest = bruh[0]
+for i in range(12):
+    if bruh[i]<smallest:
+        smallest = bruh[i]
+
+print(bruh)
+print("Smallest: ", smallest)
+
+#bruh.sort()
+#biggest = bruh[11]
+
+bruh.sort(reverse = True)
+print("Reversed: ", bruh)
+print()
+print()
+bruh.sort()
+print(bruh)
+for i in range(10):
+    if bruh[i]+1 == bruh[i+1]:
+        if bruh[i+1]+1 == bruh[i+2]:
+            print("The list contains ", bruh[i], bruh[i+1], bruh[i+2])
+
